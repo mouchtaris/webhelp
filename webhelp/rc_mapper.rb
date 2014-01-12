7e882b9f7290a31e39df3e70ae5083e93018ba78
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-01-12 22:31:34 +0200

    Retry after db-reloading if rc is not found

--- webhelp/rc_mapper.rb
+++ webhelp/rc_mapper.rb
@@ -3,16 +3,25 @@ module Webhelp
 class RcMapper < BasicObject
 
   def initialize yaml_db_file_path = 'rc.yaml'
-    @map = ::YAML.load ::File.read yaml_db_file_path
+    @yaml_db_file_path = yaml_db_file_path.to_s.freeze
+    reload!
   end
 
   def method_missing name
-    result = @map[name.to_s]
-    ::Kernel.raise "Rc not found: #{name}" unless result
+    unless result = @map[name.to_s] then
+      reload!
+      result = @map[name.to_s]
+      ::Kernel.raise "Rc not found: #{name}" unless result
+    end
     result
   end
 
   alias send method_missing
+
+  private
+  def reload!
+    @map = ::YAML.load ::File.read @yaml_db_file_path
+  end
 
 end
 
