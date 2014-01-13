ee66de914fa6e62c39b7a3a8bf99b1e9f89a5140
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-01-13 15:12:29 +0200

    Use Util::ReloadMapper

--- webhelp/rc_mapper.rb
+++ webhelp/rc_mapper.rb
@@ -3,31 +3,16 @@ require 'yaml'
 module Webhelp
 
 class RcMapper
+  include Util::ReloadingMapper
 
   DefaultYamlDbPathname = 'rc.yaml'
 
   def initialize yaml_db_file_path = DefaultYamlDbPathname
-    @yaml_db_file_path = (yaml_db_file_path || DefaultYamlDbPathname).to_s.freeze
-    reload!
+    super()
+    initialize_reloading_mapper (yaml_db_file_path || DefaultYamlDbPathname)
   end
 
-  def translate name
-    unless result = @map[name.to_s] then
-      reload!
-      result = @map[name.to_s]
-      ::Kernel.raise "Rc not found: #{name}" unless result
-    end
-    result
-  end
-
-  def each_name &block
-    @map.each_value &block
-  end
-
-  private
-  def reload!
-    @map = ::YAML.load ::File.read @yaml_db_file_path
-  end
+  alias translate []
 
 end
 
