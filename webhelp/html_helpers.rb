ef454d425bdca23c3d10afb17e4acb775b8d9ebf
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-02-02 02:49:32 +0200

    Allow custom hovering selectors for images

--- webhelp/html_helpers.rb
+++ webhelp/html_helpers.rb
@@ -15,19 +15,22 @@ module HtmlHelpers
     ]
   end
 
-  def cssimghover name, indent = nil
+  def cssimghover name, indent = nil, hover_selector_prefix: nil
     url_hover = get_rcmapper.translate :"#{name}_hover"
     [
       "background-image: url('#{url_hover}')"
     ]
   end
 
-  def img name, attrs = {}
-    imgid                     = html_escape get_imgidmanager[name]
-    imghoverid                = :"#{imgid}:hover"
+  def img name, attrs = {}, hover_selector_prefix: nil
+    imgid                     = :"##{html_escape get_imgidmanager[name]}"
+    imghoverid                = if hover_selector_prefix
+                                  then :"#{hover_selector_prefix} #{imgid}"
+                                  else :"#{imgid}:hover"
+                                end
     get_morecss[imgid]       ||= cssimg name
     get_morecss[imghoverid]  ||= cssimghover name
-    haml_code                 = "##{imgid}.image{attrs}"
+    haml_code                 = "#{imgid}.image{attrs}"
 
     haml haml_code, locals: {attrs: attrs}
   end
