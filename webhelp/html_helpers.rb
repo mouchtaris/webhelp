edb02beb2430a7f1ffa1e98ca625f06deb433df1
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-02-02 03:41:33 +0200

    Remove indentation, add extra_mixins option

--- webhelp/html_helpers.rb
+++ webhelp/html_helpers.rb
@@ -2,36 +2,42 @@ module Webhelp
 
 module HtmlHelpers
 
-  def cssimg name, indent = nil
+  def cssimg name
     url       = get_rcmapper.translate name
     width     = img_width name
     height    = img_height name
     [
-      "#{indent}background-image: url('#{url}')"  ,
-      "#{indent}background-repeat: no-repeat"     ,
-      "#{indent}background-position: top center"  ,
-      "#{indent}width: #{width}px"                ,
-      "#{indent}height: #{height}px"              ,
+      "background-image: url('#{url}')"  ,
+      "background-repeat: no-repeat"     ,
+      "background-position: top center"  ,
+      "width: #{width}px"                ,
+      "height: #{height}px"              ,
     ]
   end
 
-  def cssimghover name, indent = nil, hover_selector_prefix: nil
+  def cssimghover name, hover_selector_prefix: nil
     url_hover = get_rcmapper.translate :"#{name}_hover"
     [
       "background-image: url('#{url_hover}')"
     ]
   end
 
-  def img name, attrs = {}, hover_selector_prefix: nil
+  def img name, attrs = {}, hover_selector_prefix: nil, extra_mixins: nil
     imgid                     = :"##{html_escape get_imgidmanager[name]}"
     imghoverid                = if hover_selector_prefix
                                   then :"#{hover_selector_prefix} #{imgid}"
                                   else :"#{imgid}:hover"
                                 end
-    get_morecss[imgid]       ||= cssimg name
-    get_morecss[imghoverid]  ||= cssimghover name
-    haml_code                 = "#{imgid}.image{attrs}"
+    get_morecss[imgid]      ||= cssimg name
+    get_morecss[imghoverid] ||= cssimghover name
 
+    if extra_mixins then
+      for extra_mixin in Array extra_mixins do
+        get_morecss[imgid] << "@include #{extra_mixin}"
+      end
+    end
+
+    haml_code = "#{imgid}.image{attrs}"
     haml haml_code, locals: {attrs: attrs}
   end
 
