0e4bb03fba5c798d0bfa97b6ca5984755822d141
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-02-02 14:02:21 +0200

    Add OpReg, OpGet, OpRGet, #deobfuscate_css, #deobfuscate_html, #each_id_mapping, #each_class_mapping in Obfuscator

--- webhelp/obfuscator.rb
+++ webhelp/obfuscator.rb
@@ -9,6 +9,11 @@ class Obfuscator
   HexColorCatcher = /^\h{6}|\h{3}$/
   HtmlNameCatcher = /(class|id|for)(\s*=\s*["']?)(\w+)(["']?)/
 
+  OpReg   = Webhelp::IdManager.public_instance_method :[]
+  OpGet   = Webhelp::IdManager.public_instance_method :get
+  OpRGet  = Webhelp::IdManager.public_instance_method :reverse_get
+  private_constant *%i{ OpReg OpGet OpRGet }
+
   def initialize
     @ids, @classes = Array.new 2 do Webhelp::IdManager.new end
   end
@@ -17,6 +22,35 @@ class Obfuscator
   # in a CSS string with their obfuscated mappings.
   # @return [String] the obfuscated string
   def obfuscate_css str
+    css_obfuscation_operation OpGet, str
+  end
+
+  def deobfuscate_css str
+    css_obfuscation_operation OpRGet, str
+  end
+
+  # Detect class and ID names from an HTML file and
+  # obfuscate them.
+  # @return [String] the obfuscated string
+  def obfuscate_html str
+    html_obfuscation_operation OpReg, str
+  end
+
+  def deobfuscate_html str
+    html_obfuscation_operation OpRGet, str
+  end
+
+  def each_id_mapping &block
+    @ids.each &block
+  end
+
+  def each_class_mapping &block
+    @classes.each &block
+  end
+
+
+  private
+  def css_obfuscation_operation operation, str
     str.gsub CssNameCatcher do
       begin
         whole         = $~[0]
@@ -30,7 +64,7 @@ class Obfuscator
                         end
         if id =~ HexColorCatcher # caught a hex color
           then whole
-          else "#{discriminator}#{manager.get id}#{next_char}"
+          else "#{discriminator}#{operation.bind(manager).call id}#{next_char}"
         end
       rescue => e
         raise "Error for #{discriminator}#{id}: #{e}"
@@ -38,30 +72,24 @@ class Obfuscator
     end
   end
 
-  # Detect class and ID names from an HTML file and
-  # obfuscate them.
-  # @return [String] the obfuscated string
-  def obfuscate_html str
+  def html_obfuscation_operation operation, str
     str.gsub HtmlNameCatcher do
-      discriminator = $~[1]
-      id            = $~[3]
-      manager       = case discriminator.to_s.downcase
-                        when 'class' then @classes
-                        when 'id', 'for' then @ids
-                        else raise "How? #{discriminator.inspect}"
-                      end
-      mapping       = manager[id]
-      "#{$~[1]}#{$~[2]}#{mapping}#{$~[4]}"
+      begin
+        discriminator = $~[1]
+        id            = $~[3]
+        manager       = case discriminator.to_s.downcase
+                          when 'class' then @classes
+                          when 'id', 'for' then @ids
+                          else raise "How? #{discriminator.inspect}"
+                        end
+        mapping       = operation.bind(manager).call id
+        "#{$~[1]}#{$~[2]}#{mapping}#{$~[4]}"
+      rescue => e
+        raise "Error for #{discriminator}#{id}: #{e}"
+      end
     end
   end
 
-  def each_id_mapping &block
-    @ids.each &block
-  end
-
-  def each_class_mapping &block
-    @classes.each &block
-  end
 
 end#class Obfuscator
 
