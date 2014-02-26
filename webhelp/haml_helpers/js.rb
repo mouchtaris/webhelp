4d91bb8a74bd81298ec6e43fcf48b9030d02f9a8
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-02-26 20:45:44 +0200

    Add literal javascript generating block in js_import

--- webhelp/haml_helpers/js.rb
+++ webhelp/haml_helpers/js.rb
@@ -21,23 +21,34 @@ module Js
 
     module Development
       def js_import *ids
-        haml ids.map { |id|
-            src = Js.const_get(id).gsub '|', '\|'
-            "%script{src: %Q|#{src}|}"
-          }.
-          join "\n"
+        js_srcs =
+            ids.map do |id|
+              src = Js.const_get(id).gsub '|', '\|'
+              "%script{src: %Q|#{src}|}"
+            end.
+            join "\n"
+        literal_lines =
+            if block_given? then
+              yield.
+                  each_line.
+                  to_a.
+                  map(&'  '.method(:+)).
+                  join
+            end
+        haml "#{js_srcs}\n:javascript\n#{literal_lines}"
       end
     end#module Development
 
     module Test
       def js_import *ids
         source =
-        ids.map do |id|
-          source_basename = Js.const_get id
-          source_pathname = config.public_dir + source_basename
-          source_pathname.read
-        end.
-        join
+            ids.map do |id|
+              source_basename = Js.const_get id
+              source_pathname = config.public_dir + source_basename
+              source_pathname.read
+            end.
+            join
+        source += yield if block_given?
         "<script>#{source}</script>"
       end
     end#module Test
@@ -46,8 +57,8 @@ module Js
       define_method :js_import__test, Test.instance_method(:js_import)
       # @param ids [:Opal, :JQuery]
       #
-      def js_import *ids
-        js_import__test *ids.map { |id| :"#{id}Min" }
+      def js_import *ids, &block
+        js_import__test *ids.map { |id| :"#{id}Min" }, &block
       end
     end#module Default
 
