25461d762ab35328cbe72b33cbbe3ff039cc26eb
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-03-02 22:03:05 +0200

    Use separate img() helper for Development env

--- webhelp/haml_helpers/html.rb
+++ webhelp/haml_helpers/html.rb
@@ -6,18 +6,48 @@ module Html
 
   InstanceRequirements = %i[ rcmapper imagemetadata imageidmanager htmlimg ]
 
-  # Produce html code for an image element.
-  #
-  # @param rc_id [Symbol] the logical name of the image
-  #   resource id
-  # @param id [Symbol?] the html id of the generated
-  #   html element
-  def img rc_id, id: nil
-    url         = rcmapper.translate rc_id
-    width       = imagemetadata.width rc_id
-    height      = imagemetadata.height rc_id
-    element_id  = id || imageidmanager.next!
-    htmlimg.img element_id, url, width, height
+  def __element rc_id, id, width, height
+    [
+      (id     or imageidmanager.next!         ),
+      (width  or imagemetadata.width? rc_id   ),
+      (height or imagemetadata.height? rc_id  ),
+    ]
+  end
+
+  module Environments
+
+    module Default
+
+      # Produce html code for an image element.
+      #
+      # @param rc_id [Symbol] the logical name of the image
+      #   resource id
+      # @param id [Symbol?] the html id of the generated
+      #   html element
+      def img rc_id, id: nil, width: nil, height: nil
+        url = rcmapper.translate rc_id
+        element_id, element_width, element_height = __element rc_id, id, width, height
+        htmlimg.img element_id, url, element_width, element_height
+      end
+
+    end
+
+    module Development
+
+      define_method :__img__default, Environments::Default.instance_method(:img)
+
+      # Same as {Default#img} except that provides a replacement
+      # "image" element in case the image rc is not found.
+      #
+      def img rc_id, id: nil, width: nil, height: nil
+        __img__default rc_id, id: id, width: width, height: height
+      rescue IndexError # rc not found
+        element_id, element_width, element_height = __element rc_id, id, width, height
+        htmlimg.generate element_id, element_width, element_height
+      end
+
+    end
+
   end
 
 end#module Html
